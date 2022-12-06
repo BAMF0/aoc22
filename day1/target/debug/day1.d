8a75424bdf591678cd0c5242ab58d2cb38a4dda7
Simon Johnsson <simzter@outlook.com>
2022-12-06 10:16:03 +0100

    Remove target directory

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/simjohn/dev/aoc/day1/target/debug/day1: /home/simjohn/dev/aoc/day1/src/main.rs
--- day1/target/debug/deps/day1-b51d41f788db56e3.d
+++
@@ -1,5 +0,0 @@
-/home/simjohn/dev/aoc/day1/target/debug/deps/day1-b51d41f788db56e3.rmeta: src/main.rs
-
-/home/simjohn/dev/aoc/day1/target/debug/deps/day1-b51d41f788db56e3.d: src/main.rs
-
-src/main.rs:
